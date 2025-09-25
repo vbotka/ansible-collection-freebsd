2301fc3564da668dc3f891068acec82f60b087b0
Vladimir Botka <vbotka@gmail.com>
2025-09-25 11:24:22 +0200

    Update ug example 510 batch.sh; no cache.

--- docs/source/examples/510/batch.sh
+++ docs/source/examples/510/batch.sh
@@ -3,7 +3,7 @@
 . ../defaults/batch
 
 # destroy all jails
-VBOTKA_FREEBSD_BATCH=true ansible-playbook vbotka.freebsd.pb_iocage_destroy_all_jails.yml -i iocage.ini --flush-cache
+VBOTKA_FREEBSD_BATCH=true ansible-playbook vbotka.freebsd.pb_iocage_destroy_all_jails.yml -i iocage.ini
 
 # create templates
 (cd ../208 && ansible-playbook vbotka.freebsd.pb_iocage_template.yml -i iocage.ini)
@@ -14,9 +14,9 @@ ssh admin@$iocage_04 iocage list -lt | tee out/out-01.txt
 # create project
 ansible-playbook vbotka.freebsd.pb_iocage_project_create.yml -i hosts -i iocage.ini -e debug=true | tee out/out-02.txt
 # test all jails
-ansible-playbook pb-test-all.yml -i hosts --flush-cache | tee out/out-03.txt
+ansible-playbook pb-test-all.yml -i hosts | tee out/out-03.txt
 
 # destroy project
 ansible-playbook vbotka.freebsd.pb_iocage_project_destroy.yml -i hosts -i iocage.ini -e debug=true | tee out/out-04.txt
 # test all jails
-ansible-playbook pb-test-all.yml -i hosts --flush-cache | tee out/out-05.txt
+ansible-playbook pb-test-all.yml -i hosts | tee out/out-05.txt
