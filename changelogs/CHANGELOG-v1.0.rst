0f9e5c2c44e6ccef2243fbdcfb696be7782d9bde
Vladimir Botka <vbotka@gmail.com>
2026-03-07 03:26:13 +0100

    Add example docs/source/sandbox/handy/020

--- changelogs/CHANGELOG-v1.0.rst
+++ changelogs/CHANGELOG-v1.0.rst
@@ -17,12 +17,11 @@ Major Changes
 
 Minor Changes
 -------------
-* Update the inventory plugin iocage. Add example handy/017.
-* Replace https://iocage.readthedocs.io/en/latest/ by
+* Update the inventory plugin iocag
+* Update docs. Replace https://iocage.readthedocs.io/en/latest/ by
   https://freebsd.github.io/iocage/
+* Update examples for new remote host iocage_5 (ansible_host=handy)
 * Experimental examples moved to docs/source/sandbox/handy
-* Update examples 001, 002, 003, and 501 for new remote host iocage_5
-  (ansible_host=handy)
 
 
 0.8.2
---
+++ docs/source/sandbox/handy/020/batch.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+. ../defaults/batch
+
+# Destroy jails
+VBOTKA_FREEBSD_BATCH=true ansible-playbook vbotka.freebsd.pb_iocage_destroy_all_jails.yml -i iocage.ini --flush-cache
+ssh admin@$iocage_05 sudo iocage destroy -f ansible_client
+
+# Create templates
+(cd ../202 && ansible-playbook vbotka.freebsd.pb_iocage_template.yml -i iocage.ini --flush-cache) 
+
+# Status of templates
+ssh admin@$iocage_05 sudo iocage list -lt | tee out/out-02.txt
+
+# Create jails
+ansible-playbook pb-iocage-swarms-create.yml -i iocage.ini --flush-cache | tee out/out-03.txt
+
+# Status of jails
+ssh admin@$iocage_05 sudo iocage list -l | tee out/out-05.txt
+
+# Test
+ansible-playbook pb-test-all.yml -i hosts --flush-cache | tee out/out-06.txt
+ansible-playbook pb-test-db.yml -i hosts | tee out/out-07.txt
+
+# ansible-playbook pb-iocage-swarms-destroy.yml -i iocage.ini -i hosts | tee out/out-08.txt
