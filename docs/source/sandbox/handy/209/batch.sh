f6dd8d5672c2062462a5fb7e537253aa00096b54
Vladimir Botka <vbotka@gmail.com>
2026-03-08 08:31:15 +0100

    Add sandbox example 209.

---
+++ docs/source/sandbox/handy/209/batch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+. ../defaults/batch
+
+# Destroy jails
+VBOTKA_FREEBSD_BATCH=true ansible-playbook vbotka.freebsd.pb_iocage_destroy_all_jails.yml -i iocage.ini --flush-cache
+ssh admin@$iocage_05 sudo iocage destroy -f ansible_client_apache
+
+# Create pkglist file
+# ansible-playbook pb-pkglist.yml -i iocage.ini | tee out/out-01.txt
+
+# Create template ansible_client_apache
+ansible-playbook vbotka.freebsd.pb_iocage_template.yml -i iocage.ini | tee out/out-02.txt
