5e767b65bfa4e2ec132c3fad86f320495b8e0d31
Vladimir Botka <vbotka@gmail.com>
2026-05-04 03:32:53 +0200

    Docs. Update sandbox 300

--- docs/source/sandbox/handy/300/batch.sh
+++ docs/source/sandbox/handy/300/batch.sh
@@ -12,20 +12,26 @@ VBOTKA_FREEBSD_BATCH=true ansible-playbook vbotka.freebsd.pb_iocage_destroy_all_
 
 # status of jails
 ssh admin@$iocage_05 sudo iocage list -l | tee out/out-01.txt
-ansible-inventory -i hosts --graph | tee out/out-02.txt
+# ansible-inventory -i hosts --graph | tee out/out-02.txt
 
 # display sshd rcvar
-ansible-playbook pb-test-01.yml -i hosts --flush-cache| tee out/out-03.txt
+# ansible-playbook pb-test-01.yml -i hosts --flush-cache| tee out/out-03.txt
+
 # display dictionary jid_rcvar
 ansible-playbook pb-test-02.yml -i iocage.ini -e debug=true | tee out/out-04.txt
+
 # display enabled services
 ansible-playbook pb-test-03.yml -i iocage.ini -e debug=true | tee out/out-05.txt
+
 # display sshd status
 ansible-playbook pb-test-04.yml -i iocage.ini | tee out/out-06.txt
+
 # display sshd commands synopsis
 ansible-playbook pb-test-05.yml -i iocage.ini | tee out/out-07.txt
+
 # display sendmail rcvars
-ansible-playbook pb-test-06.yml -i hosts | tee out/out-08.txt
+# ansible-playbook pb-test-06.yml -i hosts | tee out/out-08.txt
+
 # start apcupsd
 ansible-playbook pb-test-07.yml -i iocage.ini -e debug=true | tee out/out-09.txt
 
