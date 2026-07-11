b7855ad4e2942c1b3f006b0a8293526e403d3b00
Vladimir Botka <vbotka@gmail.com>
2026-07-11 15:47:27 +0200

    Docs. Update sandbox 207.

--- docs/source/sandbox/207/batch.sh
+++ docs/source/sandbox/207/batch.sh
@@ -12,13 +12,17 @@ VBOTKA_FREEBSD_BATCH=true ansible-playbook vbotka.freebsd.pb_iocage_destroy_all_
 (cd ../202 && ansible-playbook vbotka.freebsd.pb_iocage_template.yml -i iocage.ini --flush-cache)
 
 # List templates
-ssh admin@$iocage_05 iocage list -lt | tee out/out-03.txt
+ssh admin@iocage_06 iocage list -lt | tee out/out-03.txt
 
 # Create test jails
-ssh admin@$iocage_05 sudo iocage create -n test_5 -r 15.0-RELEASE
+ssh admin@iocage_06 sudo iocage create -n test_5 -r 15.1-RELEASE
 
 # Create project
 ansible-playbook -i hosts -i iocage.ini vbotka.freebsd.pb_iocage_project_create.yml -e debug=true | tee out/out-04.txt
+
+# Status of jails
+ssh admin@iocage_06 sudo iocage list -l | tee out/out-08.txt
+ansible-inventory -i hosts --graph  --flush-cache | tee out/out-09.txt
 
 # Test all jails
 # ansible-playbook -i hosts pb-test-all.yml --flush-cache | tee out/out-05.txt
