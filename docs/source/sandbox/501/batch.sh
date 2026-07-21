753d0cb71b8162831b5e3a87163919f2e9702c00
Vladimir Botka <vbotka@gmail.com>
2026-07-21 16:04:39 +0200

    Upgrade role postinstall to 2.9.2
    * Docs. Update sandbox 501.

--- docs/source/sandbox/501/batch.sh
+++ docs/source/sandbox/501/batch.sh
@@ -6,7 +6,7 @@ ansible-playbook -i iocage.ini pb-login.yml | tee out/out-01.txt
 ansible-playbook -i iocage.ini pb-packages.yml | tee out/out-02.txt
 ansible-playbook -i iocage.ini pb-loader.yml | tee out/out-03.txt
 ansible-playbook -i iocage.ini pb-network.yml | tee out/out-04.txt
-ansible-playbook -i iocage.ini pb-pf.yml | tee out/out-05.txt
+# ansible-playbook -i iocage.ini pb-pf.yml | tee out/out-05.txt
 ansible-playbook -i iocage.ini pb-zfs.yml | tee out/out-06.txt
 ansible-playbook -i iocage.ini -t freebsd_iocage_activate -e freebsd_iocage_activate=true -e freebsd_iocage_debug=true pb-iocage.yml | tee out/out-07.txt
 # iocage fetch --release 15.1-RELEASE | tee out/out-08.txt
