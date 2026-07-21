7f6c5f856e279777278842d8145b3b5671210d80
Vladimir Botka <vbotka@gmail.com>
2026-07-21 13:47:47 +0200

    Docs. Update sandbox 501.

--- docs/source/sandbox/501/batch.sh
+++ docs/source/sandbox/501/batch.sh
@@ -2,13 +2,13 @@
 
 . ../defaults/batch
 
-ansible-playbook pb-login.yml -i iocage.ini | tee out/out-01.txt
-ansible-playbook pb-packages.yml -i iocage.ini | tee out/out-02.txt
-ansible-playbook pb-loader.yml -i iocage.ini | tee out/out-03.txt
-ansible-playbook pb-network.yml -i iocage.ini | tee out/out-04.txt
-ansible-playbook pb-pf.yml -i iocage.ini | tee out/out-05.txt
-ansible-playbook pb-zfs.yml -i iocage.ini | tee out/out-06.txt
-ansible-playbook pb-iocage.yml -i iocage.ini -t freebsd_iocage_activate -e freebsd_iocage_activate=true -e freebsd_iocage_debug=true | tee out/out-07.txt
-# iocage fetch --release 15.0-RELEASE | tee out/out-08.txt
-ansible-playbook pb-iocage.yml -i iocage.ini -t freebsd_iocage_sanity | tee out/out-09.txt
-ANSIBLE_DISPLAY_OK_HOSTS=false ansible-playbook pb-all.yml -i iocage.ini | tee out/out-10.txt
+ansible-playbook -i iocage.ini pb-login.yml | tee out/out-01.txt
+ansible-playbook -i iocage.ini pb-packages.yml | tee out/out-02.txt
+ansible-playbook -i iocage.ini pb-loader.yml | tee out/out-03.txt
+ansible-playbook -i iocage.ini pb-network.yml | tee out/out-04.txt
+ansible-playbook -i iocage.ini pb-pf.yml | tee out/out-05.txt
+ansible-playbook -i iocage.ini pb-zfs.yml | tee out/out-06.txt
+ansible-playbook -i iocage.ini -t freebsd_iocage_activate -e freebsd_iocage_activate=true -e freebsd_iocage_debug=true pb-iocage.yml | tee out/out-07.txt
+# iocage fetch --release 15.1-RELEASE | tee out/out-08.txt
+ansible-playbook -i iocage.ini -t freebsd_iocage_sanity pb-iocage.yml | tee out/out-09.txt
+ANSIBLE_DISPLAY_OK_HOSTS=false ansible-playbook -i iocage.ini pb-all.yml | tee out/out-10.txt
