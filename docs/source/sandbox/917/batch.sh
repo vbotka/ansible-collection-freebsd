14a9c66b29f15ed3dde4c8b3a3d88a37c2b26f47
Vladimir Botka <vbotka@gmail.com>
2026-07-22 14:33:00 +0200

    Docs. Update sandbox 917. Add 311.

--- docs/source/sandbox/917/batch.sh
+++ docs/source/sandbox/917/batch.sh
@@ -3,18 +3,18 @@
 . ../defaults/batch
 
 # Stop repos and repos-devel
-ssh admin@$iocage_05 sudo iocage stop repos
-ssh admin@$iocage_05 sudo iocage stop repos-devel
+# ssh admin@iocage_06 sudo iocage stop repos
+# ssh admin@iocage_06 sudo iocage stop repos-devel
 
 # Destroy repos and repos-devel
-ssh admin@$iocage_05 sudo iocage destroy -f repos
-ssh admin@$iocage_05 sudo iocage destroy -f repos-devel
+ssh admin@iocage_06 sudo iocage destroy -f repos
+ssh adminiocage_06 sudo iocage destroy -f repos-devel
 
 # Create template
 ansible-playbook pb-iocage-template.yml -i iocage.ini | tee out/out-01.txt
 
 # List templates
-ssh admin@$iocage_05 sudo iocage list -lt | tee out/out-02.txt
+ssh admin@iocage_06 sudo iocage list -lt | tee out/out-02.txt
 
 # Create the project
 ansible-playbook vbotka.freebsd.pb_iocage_project_create_from_templates.yml -i iocage.ini -i hosts | tee out/out-03.txt
@@ -23,10 +23,10 @@ ansible-playbook vbotka.freebsd.pb_iocage_project_create_from_templates.yml -i i
 ansible-inventory -i hosts --graph | tee out/out-04.txt
 
 # List jails
-ssh admin@$iocage_05 sudo iocage list -l | tee out/out-05.txt
+ssh admin@iocage_06 sudo iocage list -l | tee out/out-05.txt
 
 # Clone repos
 ansible-playbook pb-repos.yml -i hosts | tee out/out-06.txt
 
 # List repos
-ssh admin@$iocage_05 sudo iocage exec repos ls -la /usr/local/git | tee out/out-07.txt
+ssh admin@iocage_06 sudo iocage exec repos ls -la /usr/local/git | tee out/out-07.txt
