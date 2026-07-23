5e0a4b5978226d62f7cada62d9723a1b325055fa
Vladimir Botka <vbotka@gmail.com>
2026-07-23 13:27:20 +0200

    Docs. Update sandbox 921.

--- docs/source/sandbox/921/README.md
+++ docs/source/sandbox/921/README.md
@@ -4,3 +4,28 @@ Extends example 918.
 
 Test the port sysutils/ansible-init-firstboot
 Install package ansible-init-firstboot-1.0.pkg
+
+## Test the new port
+
+1) Prepare Your Local Ports Tree. Make sure you have a local clone of the
+   FreeBSD ports Git repository. If you haven't already cloned it:
+   
+   git clone https://git.FreeBSD.org/ports.git ~/freebsd-ports
+   cd ~/freebsd-ports
+
+2) Download the Patch. Go to the Bugzilla PR:
+   https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=296507. Locate the patch
+   attachment and download the raw patch file (e.g., save it as
+   ansible-init-firstboot.patch in your home directory or working directory).
+
+3) Apply the Patch with Git from the root of your ports tree ~/freebsd-ports
+
+   git am /path/to/ansible-init-firstboot.patch
+   Applying: sysutils/ansible-init-firstboot: New port: Firstboot ansible-pull initialization rc script
+
+4) Create the package
+
+   cd freebsd-portswork/pkg/ansible-init-firstboot-1.0.pkg/sysutils/ansible-init-firstboot/
+   make package
+   
+   Use the package work/pkg/ansible-init-firstboot-1.0.pkg
--- docs/source/sandbox/921/batch.sh
+++ docs/source/sandbox/921/batch.sh
@@ -3,19 +3,21 @@
 . ../defaults/batch
 
 # Stop jails foo and bar
-ssh admin@$iocage_05 sudo iocage stop foo
-ssh admin@$iocage_05 sudo iocage stop bar
+# ssh admin@iocage_06 sudo iocage stop foo
+# ssh admin@iocage_06 sudo iocage stop bar
+
 # Destroy jails foo and bar
-ssh admin@$iocage_05 sudo iocage destroy -f foo
-ssh admin@$iocage_05 sudo iocage destroy -f bar
+ssh admin@iocage_06 sudo iocage destroy -f foo
+ssh admin@iocage_06 sudo iocage destroy -f bar
+
 # Destroy template ansible-init-devel
-# ssh admin@$iocage_05 sudo iocage destroy -f ansible-init-devel
+ssh admin@iocage_06 sudo iocage destroy -f ansible-init-devel
 
 # Create template
 ansible-playbook pb-iocage-template.yml -i iocage.ini | tee out/out-01.txt
 
 # List templates
-ssh admin@$iocage_05 sudo iocage list -lt | tee out/out-02.txt
+ssh admin@iocage_06 sudo iocage list -lt | tee out/out-02.txt
 
 # Create the project
 ansible-playbook vbotka.freebsd.pb_iocage_project_create_from_templates.yml -i iocage.ini -i hosts | tee out/out-03.txt
@@ -24,9 +26,9 @@ ansible-playbook vbotka.freebsd.pb_iocage_project_create_from_templates.yml -i i
 ansible-inventory -i hosts --graph | tee out/out-04.txt
 
 # List jails
-ssh admin@$iocage_05 sudo iocage list -l | tee out/out-05.txt
+ssh admin@iocage_06 sudo iocage list -l | tee out/out-05.txt
 
 # Test (if at=now set sleep 90)
 sleep 5
-ssh admin@$iocage_05 sudo iocage exec foo "cat /tmp/ansible-hello-world.txt" | tee out/out-06.txt
-ssh admin@$iocage_05 sudo iocage exec bar "cat /tmp/ansible-hello-world.txt" | tee out/out-07.txt
+ssh admin@iocage_06 sudo iocage exec foo "cat /tmp/ansible-hello-world.txt" | tee out/out-06.txt
+ssh admin@iocage_06 sudo iocage exec bar "cat /tmp/ansible-hello-world.txt" | tee out/out-07.txt
