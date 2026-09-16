b1f8a92ddab71907ec7e07fb72538cd33b7bc7f2
Vladimir Botka <vbotka@gmail.com>
2026-09-16 15:55:26 +0200

    Docs. Update playbook for example configuration.
    * Update examples.

--- docs/source/examples/400/batch.sh
+++ docs/source/examples/400/batch.sh
@@ -3,11 +3,11 @@
 # shellcheck disable=SC1091
 . ../defaults/batch
 
-ansible-playbook pb-loader.yml -i iocage.ini | tee out/out-01.txt
+ansible-playbook -i iocage.ini pb-loader.yml | tee out/out-01.txt
 
-ansible-playbook pb-zfs.yml -i iocage.ini -t fzfs_debug -e fzfs_debug=true | tee out/out-02.txt
-ansible-playbook pb-zfs.yml -i iocage.ini | tee out/out-03.txt
-ansible-playbook pb-zfs.yml -i iocage.ini -t fzfs_facts_pools -e fzfs_debug=true | tee out/out-04.txt
-ansible-playbook pb-zfs.yml -i iocage.ini -t fzfs_facts_ds -e fzfs_facts_ds=true -e fzfs_debug=true | tee out/out-05.txt
+ansible-playbook -i iocage.ini -t fzfs_debug -e fzfs_debug=true pb-zfs.yml | tee out/out-02.txt
+ansible-playbook -i iocage.ini pb-zfs.yml | tee out/out-03.txt
+ansible-playbook -i iocage.ini -t fzfs_facts_pools -e fzfs_debug=true pb-zfs.yml | tee out/out-04.txt
+ansible-playbook -i iocage.ini -t fzfs_facts_ds -e fzfs_facts_ds=true -e fzfs_debug=true pb-zfs.yml | tee out/out-05.txt
 
 # ANSIBLE_DISPLAY_OK_HOSTS=false 
